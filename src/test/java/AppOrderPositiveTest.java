aed09c573ca8fc15b7a8e2f234f75f4779fa42e4
Anastasia Baburova <nas3235@yandex.ru>
2024-12-30 14:12:49 +0300

    добавлена задача 2

--- src/test/java/AppOrderPositiveTest.java
+++ src/test/java/AppOrderPositiveTest.java
@@ -45,7 +45,44 @@ public class AppOrderPositiveTest {
         WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
         String actualText = actualElement.getText().trim();
         assertEquals(
-        "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+                "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
         assertTrue(actualElement.isDisplayed());
+    }
+
+    @Test
+    public void shouldBeSuccessfulFormName() {
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78007777777");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=name].input_invalid.input__sub")) .getText().trim());
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=name].input_invalid.input__sub")).isDisplayed());
+    }
+
+    @Test
+    public void shouldBeSuccessfulFormPhone() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле обязательно для заполнения",
+                driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")) .getText().trim());
+    }
+
+    @Test
+    public void shouldBeSuccessfulFormPhone1() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+7800777");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertEquals("Поле заполнено не полностью, должно быть 11 символов",
+                driver.findElement(By.cssSelector("[data-test-id=phone].input_invalid .input__sub")) .getText().trim());
+    }
+
+    @Test
+    public void shouldBeSuccessfulFormAgreement() {
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+78007777777");
+        driver.findElement(By.cssSelector("button.button")).click();
+        assertTrue(driver.findElement(By.cssSelector("[data-test-id=agreement].input_invalid")) .isDisplayed());
     }
 }
