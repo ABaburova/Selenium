2159153739acfdf8d59f1e1c91dc7745022b3ced
Anastasia Baburova <nas3235@yandex.ru>
2024-12-30 14:49:52 +0300

    Задача 1

--- src/test/java/AppOrderPositiveTest.java
+++ src/test/java/AppOrderPositiveTest.java
@@ -45,7 +45,7 @@ public class AppOrderPositiveTest {
         WebElement actualElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
         String actualText = actualElement.getText().trim();
         assertEquals(
-        "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+                "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
         assertTrue(actualElement.isDisplayed());
     }
 }
